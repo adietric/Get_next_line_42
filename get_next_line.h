f66916a3d806a1135c9e073b37f49582b2eaacb2
Aymeric DIETRICH <adietric@e3r10p18.42.fr>
2020-03-07 14:23:04 +0100

    Gnl version clean

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adietric <adietric@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/22 12:14:03 by adietric          #+#    #+#             */
+/*   Updated: 2018/11/25 18:06:09 by adietric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include "./libft/libft.h"
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
